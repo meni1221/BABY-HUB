080c1afd214f8c4cf8e1c2b56fa8be53ac36c056
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-11-20 15:21:36 +0200

    Adding handleErros and app.ts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
+import router from "./src/routers/router";
+import cookieParser from "cookie-parser";
 
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(router);
+
+
+mongoose.connect(process.env.MONGO_URI || "")
+  .then(() => {
+    console.log("Connected to MongoDB Atlas"); 
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);  
+  });
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(` listen to port http://localhost:${process.env.PORT || 8000} `);
