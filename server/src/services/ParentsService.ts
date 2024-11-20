c6617df8a1ef0351fedd0e2ae29a559c768a575a
meni_levi <1224tap@gmail.com>
2024-11-20 17:45:25 +0200

    created func add new parents

--- server/src/services/ParentsService.ts
+++ server/src/services/ParentsService.ts
@@ -1,5 +1,6 @@
 import ParentsModel from "../models/ParentsModel";
 import { handleBadRequest } from "../../utils/handleError";
+import { IParents } from "../interface/parents";
 
 const allParents = async () => {
   try {
@@ -14,4 +15,21 @@ const allParents = async () => {
   }
 };
 
-export { allParents };
+// const addNewParents = async (parentsData: IParents) => {
+//   try {
+//     if (!parentsData.email || !parentsData.password) {
+//       throw new Error("Missing required fields");
+//     }
+
+//     const newParent = new User(parentsData);
+//     newParent.password = generateUserPassword(newParent.password);
+//     await newParent.save();
+//     return newParent;
+//   } catch (error: any) {
+//     return handleBadRequest("MongoDB", error);
+//   }
+// };
+
+export { allParents, 
+    // addNewParents 
+};
