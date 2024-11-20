f01edfdaa8a620054bfcd488a34f00d0c5056a5e
meni_levi <1224tap@gmail.com>
2024-11-20 16:01:21 +0200

    update type address

--- server/src/interface/parents.ts
+++ server/src/interface/parents.ts
@@ -1,9 +1,11 @@
-export interface IParents extends Document{
-    name:string
-    amount:number
-    address:string
-    phone:number
-    budget:number
-    email:string
-    isAdmin:boolean
+import { IAddress } from "./address";
+
+export interface IParents extends Document {
+  name: string;
+  amount: number;
+  address: IAddress;
+  phone: number;
+  budget: number;
+  email: string;
+  isAdmin: boolean;
 }
