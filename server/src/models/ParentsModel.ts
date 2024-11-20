0f60bd4b1ef7f79129eaffa0245bfbcd21f6c775
meni_levi <1224tap@gmail.com>
2024-11-20 16:19:34 +0200

    update adderss type

---
+++ server/src/models/ParentsModel.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+import { IParents } from "../interface/parents";
+import AdderssSchema from "../models/AddressModel";
+
+const ParentsSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  amount: {
+    type: Number,
+    require: true,
+    minlength: 1,
+    maxlength: 21
+  },
+  address: {
+    type: {AdderssSchema},
+    require: true,
+  },
+  phone: {
+    type: Number,
+    require: true,
+  },
+  budget: {
+    type: Number,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    require: true,
+  },
+});
+
+export default mongoose.model<IParents>("parents", ParentsSchema);
