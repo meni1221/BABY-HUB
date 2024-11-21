e6a893e522d12eacf1f3bbd995ab50f7af99cc79
meni_levi <1224tap@gmail.com>
2024-11-21 02:01:40 +0200

    Created controller from addNewParents

--- server/src/controllers/ParentsController.ts
+++ server/src/controllers/ParentsController.ts
@@ -1,5 +1,5 @@
 import express, { IRouter, Request, Response } from "express";
-import { allParents } from "../services/ParentsService";
+import { allParents, addNewParents } from "../services/ParentsService";
 import { handleError } from "../../utils/handleError";
 
 const router: IRouter = express.Router();
@@ -10,6 +10,15 @@ router.get("/", async (req: Request, res: Response): Promise<void> => {
     res.json(parents);
   } catch (error: any) {
     handleError(res, error.status || 404, error.message);
+  }
+});
+
+router.post("/", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const parent = await addNewParents(req.body);
+    res.status(201).json(parent);
+  } catch (error: any) {
+    handleError(res, error.status || 400, error.message);
   }
 });
 
