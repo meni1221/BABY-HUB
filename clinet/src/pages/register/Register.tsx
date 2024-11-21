1ec7630965ab6976272be4c15e1ae02723aaed0e
eliyaLevi <el0547135208@gmail.com>
2024-11-21 23:23:20 +0200

    pull

--- clinet/src/pages/register/Register.tsx
+++ clinet/src/pages/register/Register.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { RegisterParent } from "./registerParent/RegisterParent";
 import { RegisterBaybisitter } from "./registerBaybisitter/RegisterBaybisitter";
+import PageHeader from "../../componnets/PageHeader";
 
 export const Register = () => {
   const [flag, setFlag] = useState("");
 
   return (
     <>
+      <PageHeader
+        title="register"
+        subtitle="Welcome to the parent/babysitter registration page"
+      />
       <div>
         <button onClick={() => setFlag("parent")}>register parent</button>
         <button onClick={() => setFlag("babysitter")}>
