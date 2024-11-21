9e1dd04f94866a66cad9d0a2b2117134afbd5c2d
meni_levi <1224tap@gmail.com>
2024-11-21 02:09:29 +0200

    Adding router from parents

--- server/src/routers/router.ts
+++ server/src/routers/router.ts
@@ -1,13 +1,12 @@
-import express, { IRouter, NextFunction, Request, Response }  from 'express'
-import { handleError } from '../../utils/handleError';
+import express, { IRouter, NextFunction, Request, Response } from "express";
+import { handleError } from "../../utils/handleError";
+import ParentsControler from "../controllers/ParentsController";
+const router: IRouter = express.Router();
 
-const router:IRouter = express.Router()
+router.use("/parents", ParentsControler);
 
+router.use((req: Request, res: Response) => {
+  handleError(res, 404, "Not found");
+});
 
-
-router.use((req:Request,res:Response)=>{
-handleError(res,404,"Not found")
-})
-
-
-export default router
+export default router;
