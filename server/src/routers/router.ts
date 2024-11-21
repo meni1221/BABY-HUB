9924e81f6c590f6e3e8aa54a22baf07f3621dafb
eliyaLevi <el0547135208@gmail.com>
2024-11-21 11:54:25 +0200

    added order router

--- server/src/routers/router.ts
+++ server/src/routers/router.ts
@@ -1,10 +1,11 @@
 import express, { IRouter, NextFunction, Request, Response }  from 'express'
 import { handleError } from '../../utils/handleError';
 import BabysitterController from "../controllers/BabysitterController"
+import orderController from "../controllers/orderController"
 
 const router:IRouter = express.Router()
 
-
+router.use("/orders", orderController)
 router.use("/babysitter", BabysitterController)
 router.use((req:Request,res:Response)=>{
 handleError(res,404,"Not found")
