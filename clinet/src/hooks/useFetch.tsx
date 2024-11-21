cc734acf92f8b50a78f3d35a2d66aee7519dc6d1
meni_levi <1224tap@gmail.com>
2024-11-21 21:52:13 +0200

    Created a DELETE method

--- clinet/src/hooks/useFetch.tsx
+++ clinet/src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 
 export default function useFetch<T>(url: string): any {
   const [data, setData] = useState<T | null>(null);
@@ -41,7 +41,7 @@ export default function useFetch<T>(url: string): any {
       const response = await fetch(`${url}/:${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
-        credentials: "include", // חשוב בשביל קבלת הקוקיז
+        credentials: "include", 
         body: JSON.stringify(body),
       });
       if (!response.ok) {
@@ -55,10 +55,21 @@ export default function useFetch<T>(url: string): any {
     }
   };
   //   --------------DELETE method--------------
-  const DELETE = async () => {
+  const DELETE = async (id: string) => {
     try {
-    } catch (error: unknown) {
-      setError((error as Error).message || "An unknown error occurred.");
+      const response = await fetch(`${url}/:${id}`, {
+        method: "POT",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", 
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`error is: ${errorData.error.message}`);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError((error as Error).message);
     }
   };
   return { data, error, GET, POST, PATCH, DELETE };
