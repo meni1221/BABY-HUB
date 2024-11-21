be639b8221928b6884e787104245df9a4e95f768
eliyaLevi <el0547135208@gmail.com>
2024-11-21 16:47:26 +0200

    added  register to Parent

---
+++ clinet/src/pages/register/registerBaybisitter/RegisterBaybisitter.tsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+
+// export interface IBabysitter extends Document {
+//     name: string;
+//     age: number;
+//     image: string;
+//     address: string;
+//     phone: string;
+//     email: string;
+//     preferences: [string];
+//     experience: string;
+//     about: string;
+//     price: number;
+//     likes: [string];
+//     budget: number;
+//     password: string
+//   }
+
+// export const RegisterBaybisitter = () => {
+
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//     const [name, setName] = useState("");
+//   return (
+
+//   )
+// }
