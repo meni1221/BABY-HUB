9132dbe073bf7a7278970a2ca79a75243fa6295e
meni_levi <1224tap@gmail.com>
2024-11-24 14:41:39 +0200

    Created Route to parent

--- clinet/src/router/AppRouter.tsx
+++ clinet/src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import HomePage from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { Register } from "../pages/register/Register";
 import AboutPage from "../pages/AboutPage";
+import ParentPage from "../pages/ParentPage";
 
 export default function AppRouter() {
   return (
@@ -12,6 +13,7 @@ export default function AppRouter() {
         <Route path="about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/parent" element={<ParentPage />} />
       </Routes>
     </div>
   );
