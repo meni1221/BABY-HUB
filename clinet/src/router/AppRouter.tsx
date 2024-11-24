1de70c3df51995955b7a843e9c9f60239b486f13
meni_levi <1224tap@gmail.com>
2024-11-24 17:40:58 +0200

    Order in code

--- clinet/src/router/AppRouter.tsx
+++ clinet/src/router/AppRouter.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-
 import HomePage from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { Register } from "../pages/register/Register";
