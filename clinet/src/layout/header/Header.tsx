7827ea6416c45e611e066d22b8a478b1fd5a0009
Miki_Levi <miclevi101@gmail.com>
2024-11-24 12:11:19 +0200

    logic

--- clinet/src/layout/header/Header.tsx
+++ clinet/src/layout/header/Header.tsx
@@ -1,18 +1,30 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 export default function Header() {
+  const {user,logout} = useContext(AuthContext) ??{}
+
+
+  
   return (
     <div>
-      <h1>Hello from header</h1>
       <header className="nav-bar">
         <div className="home-about">
           <Link to={"/"}>Home </Link>
           <Link to={"/about"}>About </Link>
-        </div>
+        </div>1
+        <h1>Hello from header</h1>
+        {!user &&
         <div className="login-register">
-          <Link to={"/login"} id="login-header">Login</Link>
+          <Link to={"/login"} id="login-header">
+            Login
+          </Link>
           <Link to={"/register"}> Register</Link>
         </div>
+}{
+  user &&  <span onClick={ ()=> logout!()}><Link to={"/"}> Register</Link></span>
+}
       </header>
     </div>
   );
--- clinet/src/pages/AboutPage.tsx
+++ clinet/src/pages/AboutPage.tsx
@@ -1,3 +1,9 @@
 export default function AboutPage() {
-  return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus explicabo tempore cumque ullam officiis cupiditate omnis consequuntur ex, laborum nihil.</p>;
+  return (
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus explicabo
+      tempore cumque ullam officiis cupiditate omnis consequuntur ex, laborum
+      nihil.
+    </p>
+  );
 }
