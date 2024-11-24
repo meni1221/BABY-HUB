0691fec7f39b42a77b6bf5b4ffba92046e592323
meni_levi <1224tap@gmail.com>
2024-11-24 14:42:30 +0200

    View babysitter details

--- clinet/src/pages/ParentPage.tsx
+++ clinet/src/pages/ParentPage.tsx
@@ -20,12 +20,17 @@ export default function ParentPage() {
         {babysitter && babysitter.length > 0 ? (
           babysitter.map((user) => (
             <div key={user.email} className="user-card">
+                <h2>{user.name}</h2>
               <img
                 src={user.image || "default-avatar.jpg"}
                 alt={`${user}'s avatar`}
                 className="user-avatar"
               />
-              <h3>{user.name}</h3>
+              <p> {user.age}</p>
+              <p> {user.address}</p>
+              <p>{user.about}</p>
+              <p> {user.preferences}</p>
+              <p> {user.experience}</p>
               <button>להזמנה</button>
             </div>
           ))
