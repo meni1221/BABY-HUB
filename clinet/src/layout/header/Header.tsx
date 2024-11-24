dd94d74c2b5b3918178b82347e6f0d1838b173e9
Miki_Levi <miclevi101@gmail.com>
2024-11-24 11:49:44 +0200

    titel from header

--- clinet/src/layout/header/Header.tsx
+++ clinet/src/layout/header/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
+      <h1>Hello from header</h1>
       <header className="nav-bar">
         <div className="home-about">
           <Link to={"/"}>Home </Link>
