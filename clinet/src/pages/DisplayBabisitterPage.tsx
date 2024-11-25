61caf0bce246ef069657ef93afc28df64d20e8b8
meni_levi <1224tap@gmail.com>
2024-11-25 14:58:48 +0200

    Add display

--- clinet/src/pages/DisplayBabisitterPage.tsx
+++ clinet/src/pages/DisplayBabisitterPage.tsx
@@ -1,24 +1,58 @@
-// import React, { useContext } from 'react'
-// import { AuthContext } from '../providers/AuthProvider';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import useFetch from "../hooks/useFetch";
+import IBabysitter from "../interface/BabySitter";
+import { useParams } from "react-router-dom";
 
-// export default function DisplayBabisitterPage(id:string) {
-// const { user } = useContext(AuthContext) ?? {};
+export default function DisplayBabisitterPage() {
+  const { user } = useContext(AuthContext) ?? {};
+  const { GETOne, data } = useFetch("http://localhost:7700/babysitter");
+  const [babysitter, setbabysitter] = useState<IBabysitter>();
+  const { id } = useParams();
 
-//     return (
-//     <>
-//       <img
-//                 src={user.image || "default-avatar.jpg"}
-//                 alt={`${user.name}'s avatar`}
-//                 className="user-avatar"
-//               />
+  useEffect(() => {
+    GETOne(id);
+  }, []);
+  useEffect(() => {
+    if (data) setbabysitter(data);
+  }, [data]);
 
-//               <h2>{user.name}</h2>
-//               <p>
-//                 <strong>Age:</strong> {user.age}
-//               </p>
-//               <p>
-//                 <strong>Location:</strong> {user.address}
-//               </p>
-//     </>
-//   )
-// }
+  return (
+    <>
+      {babysitter ? (
+        <>
+          <img
+            src={babysitter.image || "default-avatar.jpg"}
+            alt={`${babysitter.name}'s avatar`}
+            className="user-avatar"
+          />
+
+          <h2>{babysitter.name}</h2>
+          <p>
+            <strong>Age:</strong> {babysitter.age}
+          </p>
+          <p>
+            <strong>Location:</strong> {babysitter.address}
+          </p>
+          <p>
+            <strong>preferences:</strong> {babysitter.preferences}
+          </p>
+          <p>
+            <strong>experience:</strong> {babysitter.experience}
+          </p>
+          <p>
+            <strong>about:</strong> {babysitter.about}
+          </p>
+          <p>
+            <strong>price:</strong> {babysitter.price}
+          </p>
+          <p>
+            <strong>likes:</strong> {babysitter.likes}
+          </p>
+        </>
+        
+      ) :
+      <h1>No Users</h1>}
+    </>
+  );
+}
