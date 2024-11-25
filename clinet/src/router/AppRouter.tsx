2c5a781ff940365f43f109c47ed4f19bb8176efb
Miki_Levi <miclevi101@gmail.com>
2024-11-25 10:23:04 +0200

    Add response logic to caregivers and renaming a file

--- clinet/src/router/AppRouter.tsx
+++ clinet/src/router/AppRouter.tsx
@@ -7,6 +7,8 @@ import AboutPage from "../pages/AboutPage";
 import ParentPage from "../pages/ParentPage";
 import { BaybisitterHomePage } from "../pages/BaybisitterHomePage";
 import { EditBabysitter } from "../componnets/EditBabysitter";
+import Comment from "../componnets/Comment";
+import CommentRejister from "../pages/CommentRegister";
 
 export default function AppRouter() {
   return (
@@ -18,7 +20,8 @@ export default function AppRouter() {
         <Route path="about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
-
+        <Route path="addc" element={<Comment comment={""} star={undefined} idBabysitter={""} />}/>
+        <Route path="/comment" element={<CommentRejister />} />
         <Route path="/parent" element={<ParentPage />} />
       </Routes>
     </div>
