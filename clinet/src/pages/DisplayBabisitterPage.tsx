becc445e69889e9f5c9f7e50a4ac720970001958
meni_levi <1224tap@gmail.com>
2024-11-25 14:25:10 +0200

    Created Display Babysitter

---
+++ clinet/src/pages/DisplayBabisitterPage.tsx
@@ -0,0 +1,24 @@
+// import React, { useContext } from 'react'
+// import { AuthContext } from '../providers/AuthProvider';
+
+// export default function DisplayBabisitterPage(id:string) {
+// const { user } = useContext(AuthContext) ?? {};
+
+//     return (
+//     <>
+//       <img
+//                 src={user.image || "default-avatar.jpg"}
+//                 alt={`${user.name}'s avatar`}
+//                 className="user-avatar"
+//               />
+
+//               <h2>{user.name}</h2>
+//               <p>
+//                 <strong>Age:</strong> {user.age}
+//               </p>
+//               <p>
+//                 <strong>Location:</strong> {user.address}
+//               </p>
+//     </>
+//   )
+// }
