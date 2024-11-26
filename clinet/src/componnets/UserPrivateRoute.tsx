60863f867bc5d0fa215a4acf325f62be46a432ad
meni_levi <1224tap@gmail.com>
2024-11-26 18:03:51 +0200

    user path protection

---
+++ clinet/src/componnets/UserPrivateRoute.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import { IParents } from "../interface/parents";
+
+interface PrivateRouteProps {
+  children: JSX.Element;
+}
+
+const PrivateRouteUser: React.FC<PrivateRouteProps> = ({ children }) => {
+  const { user } = useContext(AuthContext) ?? {};
+  const userAdmin = user as IParents
+  return userAdmin.isAdmin ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRouteUser;
