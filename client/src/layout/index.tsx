e5203ae6543973b64aabf5112ac77e78cbe64807
meni_levi <1224tap@gmail.com>
2026-06-21 23:31:19 +0300

    move layout to index

--- client/src/layout/index.tsx
+++ client/src/layout/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
-import Main from "./main/Main";
+import Footer from "./footer";
+import Header from "./header";
+import Main from "./main";
 
 interface Props {
   children: ReactNode;
